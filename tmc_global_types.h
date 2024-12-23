2cff8b8c185e325a9ae68817c388fcac33f12ec6
Ben <36408599+melsbacksfriend@users.noreply.github.com>
2024-12-22 21:00:45 -0600

    Add custom char_traits specialization for u8
    This makes fstream not suck anymore and should be part of the C++ standard

--- tmc_global_types.h
+++ tmc_global_types.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
 #include <vector>
+#include <string>
+#include <fstream>
 
 using s8 = int8_t;
 using s16 = int16_t;
@@ -15,7 +17,114 @@ using f64 = double;
 using ptr = void*;
 using uchr = wchar_t;
 
+namespace std {
+template <>
+struct char_traits<u8>
+{
+    using char_type = u8;
+    using int_type = s32;
+    static void assign(char_type& c1, const char_type& c2)
+    {
+        c1 = c2;
+    }
+    static bool eq(const char_type& c1, const char_type& c2)
+    {
+        return c1 == c2;
+    }
+    static bool lt(const char_type& c1, const char_type& c2)
+    {
+        return c1 < c2;
+    }
+    static size_t length(const char_type* s)
+    {
+        size_t n = 0;
+        while(*s)
+        {
+            ++s;
+            ++n;
+        }
+        return n;
+    }
+    static int compare(const char_type* s1, const char_type* s2, size_t n)
+    {
+        for (size_t i = 0; i < n; ++i)
+        {
+            if (!eq(s1[i], s2[i]))
+            {
+                return lt(s1[i], s2[i]) ? -1 : 1;
+            }
+        }
+        return 0;
+    }
+    static const char_type* find(const char_type* s, size_t n, const char_type& a)
+    {
+        for (size_t i = 0; i < n; ++i)
+        {
+            if (eq(s[i], a))
+            {
+                return s + i;
+            }
+        }
+        return nullptr;
+    }
+    static char_type* copy(char_type* dest, const char_type* src, size_t n)
+    {
+        for (size_t i = 0; i < n; ++i)
+        {
+            dest[i] = src[i];
+        }
+        return dest;
+    }
+    static char_type* move(char_type* dest, const char_type* src, size_t n)
+    {
+        if (dest <= src || dest >= src + n)
+        {
+            copy(dest, src, n);
+        }
+        else
+        {
+            for (size_t i = n; i > 0; --i)
+            {
+                dest[i - 1] = src[i - 1];
+            }
+        }
+        return dest;
+    }
+    static char_type to_char_type(const int_type& c)
+    {
+        return static_cast<char_type>(c);
+    }
+    static int_type to_int_type(const char_type& c)
+    {
+        return static_cast<int_type>(c);
+    }
+    static const char_type* assign(char_type* b, size_t n, char_type a)
+    {
+        for (size_t i = 0; i < n; ++i)
+        {
+            b[i] = a;
+        }
+        return b;
+    }
+    static bool eq_int_type(const int_type& c1, const int_type& c2)
+    {
+        return c1 == c2;
+    }
+    static int_type eof()
+    {
+        return static_cast<int_type>(-1);
+    }
+    static int_type not_eof(const int_type& c)
+    {
+        return eq_int_type(c, eof()) ? 0 : c;
+    }
+};
+}
+
 namespace tmc {
 using u8Vec = std::vector<u8>;
 using s8Vec = std::vector<s8>;
+using ufstream = std::basic_fstream<u8>;
+using uifstream = std::basic_ifstream<u8>;
+using uofstream = std::basic_ofstream<u8>;
 }
