2391dafc4f7d7429ee257ac6f6b4067b1620e64f
Ben <36408599+melsbacksfriend@users.noreply.github.com>
2025-01-09 15:58:54 -0600

    fix errors

--- vectextra.h
+++ vectextra.h
@@ -4,8 +4,22 @@
 namespace tmc
 {
 template <class T>
-bool vecEquals(std::vector<T>& v1, std::vector<T>& v2);
+bool vecEquals(std::vector<T, std::allocator<T>>& v1, std::vector<T, std::allocator<T>>& v2)
+{
+    if (v1.size() != v2.size()) return false;
+    for (u64 i = 0; i < v1.size(); i++)
+    {
+        if (v1[i] != v2[i]) return false;
+    }
+    return true;
+}
 
 template <class T>
-void appendVecs(std::vector<T>& v1, std::vector<T>& v2);
+void appendVecs(std::vector<T, std::allocator<T>>& v1, std::vector<T, std::allocator<T>>& v2)
+{
+    for (u64 i = 0; i < v2.size(); i++)
+    {
+        v1.push_back(v2[i]);
+    }
+}
 };
